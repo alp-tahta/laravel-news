f7d9b49f86a19a62502943e17b4e766fdabecaf9
Alp Tahta <alp.tht48@gmail.com>
2025-06-24 20:09:21 +0300

    add token only access  2BH52wAHrAymR7wP3CASt

---
+++ app/Http/Middleware/EnsureTokenIsValid.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class EnsureTokenIsValid
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $token = $request->header('Authorization');
+        if ($token !== '2BH52wAHrAymR7wP3CASt') {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Yetki tokeni geçersiz.',
+            ], 401);
+        }
+
+        return $next($request);
+    }
+}
