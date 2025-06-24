a5d0bee4dba312d03038cc3a00dfa466af6b2e84
Alp Tahta <alp.tht48@gmail.com>
2025-06-24 22:23:54 +0300

    add factory for 250k fake news

--- app/Models/News.php
+++ app/Models/News.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class News extends Model
 {
+    /** @use HasFactory<\Database\Factories\NewsFactory> */
+    use HasFactory;
     protected $fillable = [
         'text',
         'image_webp',
